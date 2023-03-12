dc6654793800401f4b20d456db4d12e2edb41324
Leroy van Logchem <lr.vanlogchem@gmail.com>
2023-03-12 18:30:25 +0100

    Update get_output.php

--- slurm_statuspage/get_output.php
+++ slurm_statuspage/get_output.php
@@ -1,7 +1,7 @@
 <?php
 $output = "";
 $output .= shell_exec("date");
-$output .= shell_exec("top -n 1");
+$output .= shell_exec("top -bn 1 | head -50");
 $output .= shell_exec("uptime");
 echo '<div id="output">' . nl2br(htmlspecialchars($output)) . '</div>';
 ?>
